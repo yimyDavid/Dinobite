f3978a8f06659e608053e3fcf1d31a4f65e9a9ac
yimydavid <yimysol@gmail.com>
2018-02-03 12:11:10 -0500

    Created prefabs and scripts for spike hazards.

---
+++ Assets/Scripts/Hazards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazards : MonoBehaviour {
+
+    private Player player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            player.transform.position = new Vector2(-12.45f, 5.69f);
+        }
+    }
+}
