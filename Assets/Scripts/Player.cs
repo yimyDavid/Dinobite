fe040f94377f85b19692273ade75aa959c4c24fa
yimydavid <yimysol@gmail.com>
2018-03-17 20:27:16 -0400

    Added animation to player(rexy).

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,23 +10,43 @@ public class Player : MonoBehaviour {
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool onGround;
+    private Animator anim;
+    private int facing;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         movespeed = 2;
         jumpPower = 6;
+        facing = 1;
+        anim = GetComponent<Animator>();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+            anim.SetBool("Walking", true);
+            if(facing == 1)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            facing = 0;
         }
-        if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(movespeed, rb.velocity.y);
+            anim.SetBool("Walking", true);
+            if(facing == 0)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                facing = 1;
+            }
         }
+        else {
+            anim.SetBool("Walking", false);
+            }
         if(Input.GetKey(KeyCode.Space) && onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
