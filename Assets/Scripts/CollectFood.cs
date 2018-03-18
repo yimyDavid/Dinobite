497513c2cc6dce7208c40db19e173d1899899024
yimydavid <yimysol@gmail.com>
2018-03-17 22:36:47 -0400

    Sound effect for bites.

--- Assets/Scripts/CollectFood.cs
+++ Assets/Scripts/CollectFood.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class CollectFood : MonoBehaviour {
   
     public GameObject Bite;
     public Player player;
+    public AudioSource biteSound;
 	// Use this for initialization
 	void Start () {
         player = FindObjectOfType<Player>();
+        biteSound = FindObjectOfType<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +23,7 @@ public class CollectFood : MonoBehaviour {
     {
         if(collision.tag == "Player")
         {
+            biteSound.Play();
             player.treats++;
             Instantiate(Bite, this.transform.position, this.transform.rotation);
             Destroy(gameObject);
