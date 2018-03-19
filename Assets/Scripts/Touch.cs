e07a85ac3c4444f6dc990df006da3669fa9b0642
yimydavid <yimysol@gmail.com>
2018-03-18 21:24:09 -0400

    Player controls added.

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+
+    private Player player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+	}
+	
+    public void pressLeftArrow()
+    {
+        player.moveRight = false;
+        player.moveLeft = true;
+    }
+
+    public void pressRightArrow()
+    {
+        player.moveRight = true;
+        player.moveLeft = false;
+    }
+
+    public void releaseLeftArrow()
+    {
+        player.moveLeft = false;
+    }
+
+    public void releaseRightArrow()
+    {
+        player.moveRight = false;
+    }
+
+    public void Jump()
+    {
+        player.jump();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
