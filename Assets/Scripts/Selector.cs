ef0cfc0c5087a7d52996399d7c03ec589246bdc8
yimydavid <yimysol@gmail.com>
2018-04-16 00:05:30 -0400

    Added assets to select levels.

---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour {
+
+    public bool moveLeft;
+    public bool moveRight;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.x > -3 && (moveLeft || Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            transform.position = new Vector2(transform.position.x - (float)4.93, transform.position.y);
+            moveLeft = false;
+        }
+        else if(transform.position.x < (float)0.93 && (moveRight || Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            transform.position = new Vector2(transform.position.x + (float)4.93, transform.position.y);
+            moveRight = false;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            selectLevel();
+        }
+	}
+
+    public void selectLevel()
+    {
+
+    }
+}
