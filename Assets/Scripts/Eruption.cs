daa862c12b8fc701bf9ff3f4542ea2dc28c293b7
yimydavid <yimysol@gmail.com>
2018-04-19 00:02:57 -0400

    added images for the volcano and loop effect.

---
+++ Assets/Scripts/Eruption.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eruption : MonoBehaviour {
+
+    public GameObject volcanoEruption;
+    public float spawnTime = 4f;
+    
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnFire", spawnTime, spawnTime);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // Instantiate(volcanoEruption, volcanoEruption.transform.position, volcanoEruption.transform.rotation);
+       // SleepTimeout(5);
+    }
+
+    void SpawnFire()
+    {
+        Instantiate(volcanoEruption);
+        
+    }
+}
