05bd2dbbfc10b5e8e54aac6c5d1eae9725756f53
yimydavid <yimysol@gmail.com>
2019-01-27 14:55:06 -0500

    Created the platform prefab and level size.

--- Assets/Scripts/BackAndForth.cs
+++ Assets/Scripts/BackAndForth.cs
@@ -7,11 +7,15 @@ public class BackAndForth : MonoBehaviour {
     public double amountToMove;
     public float speed;
     private float startx;
-    private int direction;
+    private float starty;
+    public int direction;
+    private Player player;
 	// Use this for initialization
 	void Start () {
-        direction = 0;
+        //direction = 0;
         startx = gameObject.transform.position.x;
+        starty = gameObject.transform.position.y;
+        player = FindObjectOfType<Player>();
 	}
 	
 	// Update is called once per frame
@@ -33,5 +37,38 @@ public class BackAndForth : MonoBehaviour {
         {
             direction = 0;
         }
+        
+        if(gameObject.transform.position.y < starty + amountToMove && direction == 3)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + speed);
+        }
+        else if(gameObject.transform.position.y >= starty + amountToMove && direction == 3)
+        {
+            direction = 2;
+        }
+        else if(gameObject.transform.position.y > starty && direction == 2)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - speed);
+        }
+        else if(gameObject.transform.position.y <= starty && direction == 2)
+        {
+            direction = 3;
+        }
 	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            player.transform.parent = gameObject.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            player.transform.parent = null;
+        }
+    }
 }
